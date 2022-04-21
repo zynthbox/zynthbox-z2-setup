56e87f63573c574402df1d26f32f754631f1fc60
Anupam Basak <anupam.basak27@gmail.com>
2022-04-21 17:07:55 +0530

    Fix script

--- usr/share/zynthbox-z2-setup/setup_z2.sh
+++ usr/share/zynthbox-z2-setup/setup_z2.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 cp -f $SCRIPTPATH/config.txt /boot
@@ -9,9 +11,8 @@ cp -f $SCRIPTPATH/initramfs.img /boot
 cp -f $SCRIPTPATH/splash.txt /boot
 cp -f $SCRIPTPATH/zynthian-splash.png /boot
 
-cd $SCRIPTPATH/"Z2 Display"
-cp -f $SCRIPTPATH/calibration.conf /etc/X11/xorg.conf.d
-cp -fr $SCRIPTPATH//boot/overlays/* /boot/overlays
+cp -f $SCRIPTPATH/"Z2 Display"/calibration.conf /etc/X11/xorg.conf.d
+cp -fr $SCRIPTPATH/"Z2 Display"/boot/overlays/* /boot/overlays
 
 cd /boot/overlays
 dtc -I dts -O dtb -o goodix_z2.dtbo goodix_z2.dts
