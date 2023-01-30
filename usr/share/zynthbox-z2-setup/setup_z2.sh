810d3b72615a9c68d171c1f802acd74212d0b29c
Anupam Basak <anupam.basak27@gmail.com>
2023-01-30 19:27:36 +0530

    Install goodix_z2.dtbo to kernel 5.15.65 overlays folder if available

--- usr/share/zynthbox-z2-setup/setup_z2.sh
+++ usr/share/zynthbox-z2-setup/setup_z2.sh
@@ -12,6 +12,13 @@ cp -f $SCRIPTPATH/preboot-splash.jpg /boot
 cp -f $SCRIPTPATH/"Z2 Display"/calibration.conf /etc/X11/xorg.conf.d
 cp -fr $SCRIPTPATH/"Z2 Display"/boot/overlays/* /boot/overlays
 
-cd /boot/overlays
-dtc -I dts -O dtb -o goodix_z2.dtbo goodix_z2.dts
+(
+    cd /boot/overlays
+    dtc -I dts -O dtb -o goodix_z2.dtbo goodix_z2.dts
+)
 
+(
+    if [ -d /boot/5.15.65-llat-v7l+ ]; then
+        cp -fr /boot/overlays/goodix_z2.dtbo /boot/5.15.65-llat-v7l+/overlays
+    fi
+)
